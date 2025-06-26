a040730b3d24d08089f076dee7582b27ac0463c7
paragp12 <paragp12@gmail.com>
2025-06-26 20:16:57 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,17 @@
 print ("adding all ml related code here")
 
+def model1():
+    print("adding code for ml model 1")
+    # Add code for model 1 here
+
+def model2():
+    print("adding code for ml model 2")
+    # Add code for model 2 here
+
+
+def model3():
+    print("adding code for ml model 3")
+    # Add code for model 3 here
+
+
+
